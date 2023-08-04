525761b1ef098ccf8f6bf80720c8aa0e1b9e1c73
Izaaz Waskito <116268166+izaazwaskito@users.noreply.github.com>
2023-08-04 21:38:30 +0700

    uplaod vercel

--- querry.sql
+++ querry.sql
@@ -62,38 +62,3 @@ CREATE TABLE skill
     skill_name VARCHAR(255),
     wrk_id VARCHAR(255)
 );
-
-CREATE TABLE recipes
-(
-    recipes_id VARCHAR PRIMARY KEY,
-    recipes_title VARCHAR(255),
-    recipes_ingredients VARCHAR(255) ,
-    recipes_photo VARCHAR(255),
-    recipes_video VARCHAR(255),
-    recipes_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    categorys_id INT,
-    users_id VARCHAR(255)
-);
-
-CREATE TABLE comments
-(
-    comment_id INT PRIMARY KEY,
-    recipes_id VARCHAR(255),
-    users_id VARCHAR(255),
-    comment_text VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE likeds
-(
-    likeds_id VARCHAR PRIMARY KEY,
-    recipes_id VARCHAR(255),
-    users_id VARCHAR(255)
-);
-
-CREATE TABLE bookmarks
-(
-    bookmarks_id VARCHAR PRIMARY KEY,
-    recipes_id VARCHAR(255),
-    users_id VARCHAR(255)
-);
